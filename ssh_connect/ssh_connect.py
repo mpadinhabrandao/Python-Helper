4b04ba882f4847a7e0a288b0bcb728d57768c37a
Márcio Padinha Brandão <mpadinhabrandao@gmail.com>
2017-08-10 09:58:04 +0100

    Create ssh_connect.py

---
+++ ssh_connect/ssh_connect.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#sudo pip install pexpect --upgrade
+from pexpect import pxssh
+import getpass
+
+try:                                                            
+    s = pxssh.pxssh()
+    hostname = raw_input('hostname: ')
+    username = raw_input('username: ')
+    password = getpass.getpass('password: ')
+    s.login (hostname, username, password,port=7777)
+    s.sendline ('uptime')   # run a command
+    s.prompt()             # match the prompt
+    print s.before          # print everything before the prompt.
+    s.sendline ('ls -l')
+    s.prompt()
+    print s.before
+    s.sendline ('df')
+    s.prompt()
+    print s.before
+    s.logout()
+except pxssh.ExceptionPxssh, e:
+    print "pxssh failed on login."
+    print str(e)
